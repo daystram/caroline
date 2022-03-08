edf56a2282aa4aeffc1fb63a6dabef674d6f7944
Danny August Ramaputra <daystram@gmail.com>
2022-03-08 22:52:27 +1000

    feat: added music source types

--- internal/domain/music.go
+++ internal/domain/music.go
@@ -2,24 +2,58 @@ package domain
 
 import (
 	"time"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/zmb3/spotify/v2"
 )
 
 type Music struct {
 	Query            string
-	Title            string
-	URL              string
-	Thumbnail        string
-	Duration         time.Duration
 	QueuedAt         time.Time
 	QueuedByID       string
 	QueuedByUsername string
-	Loaded           bool
+
+	Source         MusicSource
+	SpotifyTrackID string
+	YouTubeVideoID string
+
+	Loaded    bool
+	Title     string
+	URL       string
+	Thumbnail string
+	Duration  time.Duration
+}
+
+type MusicSource uint
+
+const (
+	MusicSourceSpotifyPlaylist MusicSource = iota
+	MusicSourceSpotifyTrack
+	MusicSourceYouTubeVideo
+	MusicSourceSearch
+)
+
+func (s MusicSource) String() string {
+	switch s {
+	case MusicSourceSpotifyPlaylist:
+		return "Spotify Playlist"
+	case MusicSourceSpotifyTrack:
+		return "Spotify"
+	case MusicSourceYouTubeVideo:
+		return "YouTube"
+	case MusicSourceSearch:
+		return "Search"
+	default:
+		return "invalid source"
+	}
 }
 
 type MusicUseCase interface {
+	Parse(query string, user *discordgo.User) (string, []*Music, error)
 }
 
 type MusicRepository interface {
-	SearchOne(query string) (*Music, error)
+	GetSpotifyPlaylist(id string) (*spotify.FullPlaylist, []spotify.PlaylistTrack, error)
+	Load(music *Music) error
 	GetStreamURL(music *Music) (string, error)
 }
--- internal/usecase/music.go
+++ internal/usecase/music.go
@@ -1,7 +1,20 @@
 package usecase
 
 import (
+	"fmt"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+
 	"github.com/daystram/caroline/internal/domain"
+)
+
+var (
+	spotifyPlaylistRegex = regexp.MustCompile(`spotify\.com\/playlist\/(?P<playlistID>[^\?&"'>]+)`)
+	spotifyTrackRegex    = regexp.MustCompile(`spotify\.com\/track\/(?P<trackID>[^\?&"'>]+)`)
+	youtubeVideoRegex    = regexp.MustCompile(`(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))(?P<videoID>[^\?&"'>]+)`)
 )
 
 func NewMusicUseCase(musicRepo domain.MusicRepository) (domain.MusicUseCase, error) {
@@ -15,3 +28,62 @@ type musicUseCase struct {
 }
 
 var _ domain.MusicUseCase = (*musicUseCase)(nil)
+
+func (u *musicUseCase) Parse(query string, user *discordgo.User) (string, []*domain.Music, error) {
+	meta := ""
+	query = strings.TrimSpace(query)
+
+	musics := make([]*domain.Music, 0)
+	switch {
+	case spotifyPlaylistRegex.MatchString(query):
+		playlistID := spotifyPlaylistRegex.FindStringSubmatch(query)[spotifyPlaylistRegex.SubexpIndex("playlistID")]
+		p, tracks, err := u.musicRepo.GetSpotifyPlaylist(playlistID)
+		if err != nil {
+			return "", nil, err
+		}
+		for i, t := range tracks {
+			musics = append(musics, &domain.Music{
+				Query:            fmt.Sprintf("(%d) %s", i+1, query),
+				QueuedAt:         time.Now(),
+				QueuedByID:       user.ID,
+				QueuedByUsername: user.Username,
+				Source:           domain.MusicSourceSpotifyPlaylist,
+				SpotifyTrackID:   t.Track.ID.String(),
+			})
+		}
+		meta = p.Name
+
+	case spotifyTrackRegex.MatchString(query):
+		trackID := spotifyTrackRegex.FindStringSubmatch(query)[spotifyTrackRegex.SubexpIndex("trackID")]
+		musics = append(musics, &domain.Music{
+			Query:            query,
+			QueuedAt:         time.Now(),
+			QueuedByID:       user.ID,
+			QueuedByUsername: user.Username,
+			Source:           domain.MusicSourceSpotifyTrack,
+			SpotifyTrackID:   trackID,
+		})
+
+	case youtubeVideoRegex.MatchString(query):
+		videoID := youtubeVideoRegex.FindStringSubmatch(query)[youtubeVideoRegex.SubexpIndex("videoID")]
+		musics = append(musics, &domain.Music{
+			Query:            query,
+			QueuedAt:         time.Now(),
+			QueuedByID:       user.ID,
+			QueuedByUsername: user.Username,
+			Source:           domain.MusicSourceYouTubeVideo,
+			YouTubeVideoID:   videoID,
+		})
+
+	default:
+		musics = append(musics, &domain.Music{
+			Query:            query,
+			QueuedAt:         time.Now(),
+			QueuedByID:       user.ID,
+			QueuedByUsername: user.Username,
+			Source:           domain.MusicSourceSearch,
+		})
+	}
+
+	return meta, musics, nil
+}
