4aece0a45c0cba24d897cd9ddcb809e719d8bea2
Danny August Ramaputra <daystram@gmail.com>
2022-03-12 01:30:59 +1000

    refactor: fully migrated to youtube-dl for searches

--- go.mod
+++ go.mod
@@ -7,24 +7,17 @@ require (
 	github.com/daystram/dgvoice v0.0.0-20220308170814-a0065fb4d714
 	github.com/zmb3/spotify/v2 v2.0.1
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
-	google.golang.org/api v0.70.0
 )
 
 require (
-	cloud.google.com/go/compute v1.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
-	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
-	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20220218161850-94dd64e39d7c // indirect
-	google.golang.org/grpc v1.44.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	layeh.com/gopus v0.0.0-20210501142526-1ee02d434e32 // indirect
--- internal/repository/music.go
+++ internal/repository/music.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"os/exec"
 	"sort"
 	"time"
@@ -13,26 +14,16 @@ import (
 	"github.com/zmb3/spotify/v2"
 	spotifyauth "github.com/zmb3/spotify/v2/auth"
 	"golang.org/x/oauth2/clientcredentials"
-	"google.golang.org/api/option"
-	"google.golang.org/api/youtube/v3"
 
 	"github.com/daystram/caroline/internal/domain"
-	"github.com/daystram/caroline/internal/util"
 )
 
 const (
+	youtubeDLRetries  = 3
 	youtubeURLPattern = "https://youtu.be/"
 )
 
-func NewMusicRepository(ytAPIKey, spClientID, spClientSecret string) (domain.MusicRepository, error) {
-	// youtube
-	ytCtx := context.Background()
-	ytAPI, err := youtube.NewService(ytCtx, option.WithAPIKey(ytAPIKey))
-	if err != nil {
-		return nil, err
-	}
-
-	// spotify
+func NewMusicRepository(spClientID, spClientSecret string) (domain.MusicRepository, error) {
 	spCtx := context.Background()
 	config := &clientcredentials.Config{
 		ClientID:     spClientID,
@@ -42,17 +33,12 @@ func NewMusicRepository(ytAPIKey, spClientID, spClientSecret string) (domain.Mus
 	spAPI := spotify.New(config.Client(spCtx))
 
 	return &musicRepository{
-		ytAPI: ytAPI,
-		ytCtx: ytCtx,
 		spAPI: spAPI,
 		spCtx: spCtx,
 	}, nil
 }
 
 type musicRepository struct {
-	ytAPI *youtube.Service
-	ytCtx context.Context
-
 	spAPI *spotify.Client
 	spCtx context.Context
 }
@@ -98,51 +84,54 @@ func (r *musicRepository) Load(m *domain.Music) error {
 		// continue searching below
 	}
 
+	var err error
+	var resp *YouTubeDLResponse
 	if videoID == "" {
-		resp, err := r.ytAPI.Search.List([]string{"id"}).Q(m.Query).MaxResults(1).Do()
-		if err != nil {
-			return err
-		}
-		if len(resp.Items) == 0 {
-			return domain.ErrMusicNotFound
-		}
-		videoID = resp.Items[0].Id.VideoId
+		log.Println("search")
+		resp, err = execYouTubeDL(fmt.Sprintf("ytsearch1:'%s'", m.Query))
+	} else {
+		log.Println("direct")
+		resp, err = execYouTubeDL(videoID)
 	}
-
-	resp, err := r.ytAPI.Videos.List([]string{"id", "snippet", "contentDetails"}).Id(videoID).Do()
 	if err != nil {
 		return err
 	}
-	if len(resp.Items) == 0 {
+	if resp.ID == "" {
 		return domain.ErrMusicNotFound
 	}
 
-	m.Title = resp.Items[0].Snippet.Title
-	m.URL = fmt.Sprintf("%s%s", youtubeURLPattern, resp.Items[0].Id)
-	m.Thumbnail = resp.Items[0].Snippet.Thumbnails.High.Url
-	m.Duration = util.ParseYouTubeDuration(resp.Items[0].ContentDetails.Duration)
+	m.Title = resp.Title
+	m.URL = fmt.Sprintf("%s%s", youtubeURLPattern, resp.ID)
+	if len(resp.Thumbnails) > 0 {
+		m.Thumbnail = resp.Thumbnails[len(resp.Thumbnails)-1].URL
+	}
+	m.Duration = time.Duration(resp.Duration) * time.Second
 	m.Loaded = true
 
 	return nil
 }
 
 func (r *musicRepository) GetStreamURL(music *domain.Music) (string, error) {
-	v, err := GetYouTubeDL(music.URL)
+	resp, err := execYouTubeDL(music.URL)
 	if err != nil {
 		return "", err
 	}
 
-	f := filterFormats(v.Formats, "webm", "opus")
+	f := filterFormats(resp.Formats, "webm", "opus")
 	if len(f) == 0 {
 		return "", domain.ErrMusicNotFound
 	}
 	sortFormats(f)
 
-	return v.Formats[0].URL, nil
+	return resp.Formats[0].URL, nil
 }
 
 type YouTubeDLResponse struct {
-	Formats []YouTubeDLFormat `json:"formats"`
+	ID         string               `json:"id"`
+	Title      string               `json:"title"`
+	Duration   int                  `json:"duration"`
+	Formats    []YouTubeDLFormat    `json:"formats"`
+	Thumbnails []YouTubeDLThumbnail `json:"thumbnails"`
 }
 
 type YouTubeDLFormat struct {
@@ -152,26 +141,32 @@ type YouTubeDLFormat struct {
 	AvgBitrate float32 `json:"abr"`
 }
 
-func GetYouTubeDL(url string) (*YouTubeDLResponse, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+type YouTubeDLThumbnail struct {
+	URL    string `json:"url"`
+	Height int    `json:"height"`
+	Width  int    `json:"width"`
+}
+
+func execYouTubeDL(arg ...string) (*YouTubeDLResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	cmd := exec.CommandContext(ctx, "youtube-dl", "--dump-json", url)
+	cmd := exec.CommandContext(ctx, "youtube-dl", append(arg, "--dump-json")...)
 
 	var out bytes.Buffer
 	cmd.Stdout = &out
 
 	err := cmd.Run()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("youtube-dl: %w", err)
 	}
 
-	v := YouTubeDLResponse{}
-	err = json.Unmarshal(out.Bytes(), &v)
+	resp := YouTubeDLResponse{}
+	err = json.Unmarshal(out.Bytes(), &resp)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("youtube-dl: %w", err)
 	}
 
-	return &v, nil
+	return &resp, nil
 }
 
 func filterFormats(formats []YouTubeDLFormat, ext, acodec string) []YouTubeDLFormat {
