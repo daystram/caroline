bce651e444d54346435555d62011aeeed91ed61b
Danny August Ramaputra <daystram@gmail.com>
2023-04-15 07:05:40 +0000

    fix: update np message on move command

--- internal/interaction/caroline/move.go
+++ internal/interaction/caroline/move.go
@@ -130,5 +130,11 @@ func moveCommand(srv *server.Server) func(*discordgo.Session, *discordgo.Interac
 		if err != nil {
 			log.Printf("%s: %s: %s\n", i.Type, util.InteractionName(i), err)
 		}
+
+		err = srv.UC.Player.UpdateNPMessage(s, p, q, true)
+		if err != nil {
+			log.Printf("%s: %s: %s\n", i.Type, util.InteractionName(i), err)
+			return
+		}
 	}
 }
